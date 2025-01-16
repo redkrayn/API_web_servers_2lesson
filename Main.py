e4622cc20f5d2ae66ca3e7eb28194a68603ddd8f
redkrayn <redkrayn@yandex.ru>
2025-01-16 23:22:18 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import urllib.parse
 from dotenv import load_dotenv
 
 
-def shorten_link():
+def shorten_link(token, link):
     url = 'https://api.vk.ru/method/utils.getShortLink'
     payload = {"access_token": token, "v": 5.81, "url": link}
     response = requests.get(url, params=payload)
@@ -13,46 +13,43 @@ def shorten_link():
     return short_link.get("response").get("short_url")
 
 
-def count_clicks():
-    urlparse = urllib.parse.urlparse(link).path.replace('/', '')
+def count_clicks(token, link):
+    url_path = urllib.parse.urlparse(link).path.replace('/', '')
     url = 'https://api.vk.ru/method/utils.getLinkStats'
-    payload = {"access_token": token, "v": 5.81, "key": urlparse, "interval": "forever"}
+    payload = {"access_token": token, "v": 5.81, "key": url_path, "interval": "forever"}
     response = requests.get(url, params=payload)
     count_click = response.json()
     return count_click.get("response").get("stats")[0]["views"]
 
 
-def is_shorten_link():
-    urlparse = urllib.parse.urlparse(link).path.replace('/', '')
+def is_shorten_link(token, link):
+    url_path = urllib.parse.urlparse(link).path.replace('/', '')
     url = 'https://api.vk.ru/method/utils.getLinkStats'
-    payload = {"access_token": token, "v": 5.81, "key": urlparse, "interval": "forever"}
+    payload = {"access_token": token, "v": 5.81, "key": url_path, "interval": "forever"}
     response = requests.get(url, params=payload)
-    get_error = response.json()
-    if urllib.parse.urlparse(link).netloc == "vk.cc":
-        if get_error.get('error'):
-            return False
-        else:
-            return True
-    else:
-        return False
+    mistake = response.json()
+    return mistake.get('error') is None
 
 
-if __name__ == "__main__":
+def main():
     load_dotenv()
-    token = os.environ["TOKEN_VK_API"]
+    token = os.environ["VK_API_TOKEN"]
     link = input("Введите ссылку: ")
-    if is_shorten_link() == False:
+    if not is_shorten_link(token, link):
         try:
-            shorten_link()
-            print("Короткая ссылка", shorten_link())
+            print("Короткая ссылка", shorten_link(token, link))
         except AttributeError:
             print("Неверный адресс")
     else:
         try:
-            count_clicks()
-            print("Количество переходов по ссылке:", count_clicks())
+            print("Количество переходов по ссылке:", count_clicks(token, link))
         except IndexError:
             print("По ссылке ещё не переходили")
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 
